f85390b8e3ca7110a41fc15d7dd8cd9a9beb8641
aditya anand <aditya68523490@gmail.com>
2025-10-22 23:31:44 +0530

    Update app.py

--- sih/student/app.py
+++ sih/student/app.py
@@ -15,7 +15,7 @@ student_bp = Blueprint(
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Oj@svigupt@17",
+    password="aditya",
     database="adm"
 )
 cursor = db.cursor(dictionary=True)
@@ -123,3 +123,4 @@ def get_attendance(student_id):
         (student_id,)
     )
     return jsonify(cursor.fetchall())
+
